2185b4413040aad5a153acd5b10da95fb042369d
ArturoS <arturos@cern.ch>
2019-03-01 11:10:24 +0100

    title of header

--- demo-1/photos/page-photo-1.1.php
+++ demo-1/photos/page-photo-1.1.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html>
 <header>
-  <?php include('header-pages.html'); ?>
+  <?php include('../header-pages.html'); ?>
 </header>
 
 <!-- body -->
 <body>
-  <CENTRE><p>Hola! Test Page</p></CENTRE>
+  <CENTRE>
+    <p>Hola! Test landing page</p>
+    <p><a href="https://ihm.universidad.ch/instruments/index-div.html">click here to continue demo</a></p>
+  </CENTRE>
 </body>
 <!-- end main body -->
 
 <footer>
-  <?php include('footer-pages.html'); ?>
+  <?php include('../footer-pages.html'); ?>
 </footer>
 </html>
