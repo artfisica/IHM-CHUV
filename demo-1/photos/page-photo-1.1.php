9a57abdd79442284b8acf7cc6fdfc222fa14e6e8
ArturoS <arturos@cern.ch>
2019-03-02 20:06:13 +0100

    saving last changes of Friday 28/02

--- demo-1/photos/page-photo-1.1.php
+++ demo-1/photos/page-photo-1.1.php
@@ -1,19 +1,46 @@
 <!DOCTYPE html>
 <html>
 <header>
-  <?php include('../header-pages.html'); ?>
+    <!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+
+  		<meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+          <title>IHM</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta name="description" content="Thumbnail Proximity Effect with jQuery and CSS3" />
+          <meta name="keywords" content="thumbnails, jquery, proximity, effect, css3, scale, mouse, hover" />
+          <meta name="author" content="Codrops" />
+          <link rel="shortcut icon" href="../../favicon.ico">
+          <link rel="stylesheet" type="text/css" href="../css/demo.css" />
+          <link rel="stylesheet" type="text/css" href="../css/style-large.css" />
+
+      <!-- Codrops top bar -->
+      <div class="codrops-top">
+          <a href="https://www.chuv.ch/ihm/">Go to Institute page</a>
+          <span class="right">
+    			    <a href="https://commons.wikimedia.org/wiki/Category:Institut_universitaire_d%27histoire_de_la_m%C3%A9decine_et_de_la_sant%C3%A9_publique" target="_blank">wikimedia repository</a>
+  				    <a href="http://creativecommons.org/licenses/by-nc/3.0/" target="_blank">CC BY-NC 3.0</a>
+  				</span>
+          <div class="clr"></div>
+      </div><!--/ Codrops top bar -->
+
+  		<CENTER>
+        <h1 style="color:white;font-size:140%;"><img src="../images/chuv.png" alt="logo CHUV" align="centre"><b>Institut des humanités en médecine</b></h1>
+  		</CENTER>
 </header>
 
 <!-- body -->
 <body>
   <CENTRE>
     <p>Hola! Test landing page</p>
-    <p><a href="https://ihm.universidad.ch/instruments/index-div.html">click here to continue demo</a></p>
+    <p><a href="../../instruments/index-div.html">click here to continue demo</a></p>
   </CENTRE>
 </body>
 <!-- end main body -->
 
 <footer>
-  <?php include('../footer-pages.html'); ?>
+  <div class="footer">
+    <p><img src="../images/chuv.png" alt="logo CHUV" align="centre"><b>Institut des humanités en médecine</b></p>
+  </div>
 </footer>
 </html>
