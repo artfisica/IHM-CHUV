51a4bacb416a2f2f1ebbef8f55e0e8f415dbe4f0
ArturoS <arturos@cern.ch>
2019-03-23 13:43:36 +0100

    Updates Friday 22th March. PHP mainly and pictures examples.

--- demo-1/photos/page-photo-1.1.php
+++ demo-1/photos/page-photo-1.1.php
@@ -1,40 +1,10 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <?php include('head.html'); ?>
+</head>
 <header>
-    <!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
-
-  		<meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-          <title>IHM</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta name="description" content="Thumbnail Proximity Effect with jQuery and CSS3" />
-          <meta name="keywords" content="thumbnails, jquery, proximity, effect, css3, scale, mouse, hover" />
-          <meta name="author" content="Codrops" />
-          <link rel="shortcut icon" href="../../favicon.ico">
-          <link rel="stylesheet" type="text/css" href="../css/demo.css" />
-          <link rel="stylesheet" type="text/css" href="../css/style-large.css" />
-
-          <!-- Codrops top bar -->
-          <div class="codrops-top">
-      			<a href="https://www.chuv.ch/ihm/">Page de l'institut</a>
-            <span class="right">
-              <a href="https://commons.wikimedia.org/wiki/Category:Institut_universitaire_d%27histoire_de_la_m%C3%A9decine_et_de_la_sant%C3%A9_publique" target="_blank">Dépôt Wikimedia</a>
-            </span>
-              <div class="clr"></div>
-          </div><!--/ Codrops top bar -->
-
-  		<CENTER>
-        <h1 style="color:white;font-size:140%;"><img src="../images/chuv.png" alt="logo CHUV" align="centre"><b>Institut des humanités en médecine</b></h1>
-  		</CENTER>
-
-      <style>
-      p.ex1 {
-         border: 1px solid black;
-         padding: 50px;
-         margin: 50px;
-      }
-
-</style>
+  <?php include('header.html'); ?>
 </header>
 
 <!-- body -->
@@ -44,3 +14,8 @@
     </br>
 </body>
 <!-- end main body -->
+
+<footer>
+  <?php include('footer.html'); ?>
+</footer>
+</html>
---
+++ demo-1/photos/photo.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include('head.html'); ?>
+</head>
+<header>
+  <?php include('header.html'); ?>
+</header>
+
+<!-- body -->
+<body>
+  <div class="macrocontainer">
+      <div class="left">
+        <p>
+          <a href="https://commons.wikimedia.org/wiki/File:5_porte-empreinte_-_IHM-0641.jpg#/media/File:5_porte-empreinte_-_IHM-0641.jpg">
+              <?php echo "<img src=\"images/photo/" . $_GET["photo"] . ".jpg\" width=\"100%\">"; ?>
+          </a>
+        </p>
+      </div>
+      <div class="right">
+        <!-- related Books -->
+        <div class="component">
+          <ul class="align">
+            <!-- Book 1 -->
+            <li>
+              <figure class='book'>
+                <!-- Front -->
+                <ul class='hardcover_front'>
+                  <li>
+                    <div class="coverDesign grey-1">
+                      <h1>Title</h1>
+                      <p>of the book</p>
+                    </div>
+                  </li>
+                  <li></li>
+                </ul>
+                <!-- Pages -->
+                <ul class='page'>
+                  <li></li>
+                  <li>
+                    <a class="btn" href="#">read me</a>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <!-- Back -->
+                <ul class='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul class='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </figure>
+            </li>
+            <!-- Book 2 -->
+            <li>
+              <figure class='book'>
+                <!-- Front -->
+                <ul class='hardcover_front'>
+                  <li>
+                    <div class="coverDesign grey-2">
+                      <h1>Title</h1>
+                      <p>of the book</p>
+                    </div>
+                  </li>
+                  <li></li>
+                </ul>
+                <!-- Pages -->
+                <ul class='page'>
+                  <li></li>
+                  <li>
+                    <a class="btn" href="#">read me</a>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <!-- Back -->
+                <ul class='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul class='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </figure>
+            </li>
+            <!-- Book 3 -->
+            <li>
+              <figure class='book'>
+                <!-- Front -->
+                <ul class='hardcover_front'>
+                  <li>
+                    <div class="coverDesign grey-3">
+                      <h1>Title</h1>
+                      <p>of the book</p>
+                    </div>
+                  </li>
+                  <li></li>
+                </ul>
+                <!-- Pages -->
+                <ul class='page'>
+                  <li></li>
+                  <li>
+                    <a class="btn" href="#">read me</a>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <!-- Back -->
+                <ul class='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul class='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </figure>
+            </li>
+            <!-- Book 4 -->
+            <li>
+              <figure class='book'>
+                <!-- Front -->
+                <ul class='hardcover_front'>
+                  <li>
+                    <div class="coverDesign grey-4">
+                      <h1>Title</h1>
+                      <p>of the book</p>
+                    </div>
+                  </li>
+                  <li></li>
+                </ul>
+                <!-- Pages -->
+                <ul class='page'>
+                  <li></li>
+                  <li>
+                    <a class="btn" href="#">read me</a>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <!-- Back -->
+                <ul class='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul class='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </figure>
+            </li>
+            <!-- Book 5 -->
+            <li>
+              <figure class='book'>
+                <!-- Front -->
+                <ul class='hardcover_front'>
+                  <li>
+                    <div class="coverDesign grey-5">
+                      <h1>Title</h1>
+                      <p>of the book</p>
+                    </div>
+                  </li>
+                  <li></li>
+                </ul>
+                <!-- Pages -->
+                <ul class='page'>
+                  <li></li>
+                  <li>
+                    <a class="btn" href="#">read me</a>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <!-- Back -->
+                <ul class='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul class='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </figure>
+            </li>
+            <!-- Book 6 -->
+            <li>
+              <figure class='book'>
+                <!-- Front -->
+                <ul class='hardcover_front'>
+                  <li>
+                    <div class="coverDesign grey-6">
+                      <h1>Title</h1>
+                      <p>of the book</p>
+                    </div>
+                  </li>
+                  <li></li>
+                </ul>
+                <!-- Pages -->
+                <ul class='page'>
+                  <li></li>
+                  <li>
+                    <a class="btn" href="#">read me</a>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <!-- Back -->
+                <ul class='hardcover_back'>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <ul class='book_spine'>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </figure>
+            </li>
+            <!-- End of Books -->
+          </ul>
+        </div>
+      </div>
+      <div class="middle">
+        <a style="color:white;"><b>Lorem ipsum dolor sit amet, mutat viris tacimates has an</b>
+          <br>	An prima facilisis vix, eos et alia iudico, ad possim deleniti petentium pro.
+            An prima facilisis vix, eos et alia iudico, ad possim deleniti petentium pro.
+                        Iusto commune id nam, usu assum dolores definiebas no. At mel populo habemus. Legimus consectetuer te mea.
+                        Ut vel mutat soluta tibique. Duo modo suavitate delicatissimi cu, principes omittantur an vis, vim dolore.</a>
+        <a href="https://creativecommons.org/licenses/by-sa/4.0" title="Creative Commons Attribution-Share Alike 4.0">CC BY-SA 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=62479294">Link</a>
+      </div>
+      <div class="below">
+        <!-- related photos -->
+        <div class="component">
+           <div class="container demo-3">
+            <!-- Top Navigation -->
+             <ul class="grid cs-style-4">
+               <li>
+                 <figure>
+                   <div><img src="images/photo/photo-1.2.jpg" alt="img05"></div>
+                   <figcaption>
+                     <h3>Instrument #26</h3>
+                     <span>A short title for the picture</span>
+                     <a href="http://dribbble.com/shots/1116775-Safari">Take a look</a>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <div><img src="images/photo/photo-1.3.jpg" alt="img06"></div>
+                  <figcaption>
+                    <h3>Instrument #1</h3>
+                    <span>A short title for the picture</span>
+                    <a href="http://dribbble.com/shots/1118904-Game-Center">Take a look</a>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <div><img src="images/photo/photo-1.4.jpg" alt="img02"></div>
+                  <figcaption>
+                    <h3>Instrument #3</h3>
+                    <span>A short title for the picture</span>
+                    <a href="http://dribbble.com/shots/1115960-Music">Take a look</a>
+                  </figcaption>
+                </figure>
+              </li>
+              <li>
+                <figure>
+                  <div><img src="images/photo/photo-1.5.jpg" alt="img04"></div>
+                  <figcaption>
+                    <h3>Instrument #54</h3>
+                    <span>A short title for the picture</span>
+                    <a href="http://dribbble.com/shots/1116685-Settings">Take a look</a>
+                  </figcaption>
+                </figure>
+              </li>
+             </ul>
+           </div><!-- /container -->
+           <script src="js/toucheffects.js"></script>
+        </div>
+      </div>
+  </div>
+</body>
+<!-- end main body -->
+
+<footer>
+  <?php include('footer.html'); ?>
+</footer>
+</html>
