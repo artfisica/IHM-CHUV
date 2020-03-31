d226d03df105753db9f81ef4185d75d467852fc7
Arturos Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2020-03-31 12:14:10 +0200

    more updates

---
+++ demo-1/photos/books/ALL-replace.sh
@@ -0,0 +1,77 @@
+./replace.sh data-photo-1.1.txt books-matrix-1.1.html
+./replace.sh data-photo-1.2.txt books-matrix-1.2.html
+./replace.sh data-photo-1.3.txt books-matrix-1.3.html
+./replace.sh data-photo-1.4.txt books-matrix-1.4.html
+./replace.sh data-photo-1.5.txt books-matrix-1.5.html
+./replace.sh data-photo-1.6.txt books-matrix-1.6.html
+./replace.sh data-photo-1.7.txt books-matrix-1.7.html
+./replace.sh data-photo-1.8.txt books-matrix-1.8.html
+./replace.sh data-photo-1.9.txt books-matrix-1.9.html
+./replace.sh data-photo-1.10.txt books-matrix-1.10.html
+
+./replace.sh data-photo-2.1.txt books-matrix-2.1.html
+./replace.sh data-photo-2.2.txt books-matrix-2.2.html
+./replace.sh data-photo-2.3.txt books-matrix-2.3.html
+./replace.sh data-photo-2.4.txt books-matrix-2.4.html
+./replace.sh data-photo-2.5.txt books-matrix-2.5.html
+./replace.sh data-photo-2.6.txt books-matrix-2.6.html
+./replace.sh data-photo-2.7.txt books-matrix-2.7.html
+./replace.sh data-photo-2.8.txt books-matrix-2.8.html
+./replace.sh data-photo-2.9.txt books-matrix-2.9.html
+./replace.sh data-photo-2.10.txt books-matrix-2.10.html
+
+./replace.sh data-photo-3.1.txt books-matrix-3.1.html
+./replace.sh data-photo-3.2.txt books-matrix-3.2.html
+./replace.sh data-photo-3.3.txt books-matrix-3.3.html
+./replace.sh data-photo-3.4.txt books-matrix-3.4.html
+./replace.sh data-photo-3.5.txt books-matrix-3.5.html
+./replace.sh data-photo-3.6.txt books-matrix-3.6.html
+./replace.sh data-photo-3.7.txt books-matrix-3.7.html
+./replace.sh data-photo-3.8.txt books-matrix-3.8.html
+./replace.sh data-photo-3.9.txt books-matrix-3.9.html
+./replace.sh data-photo-3.10.txt books-matrix-3.10.html
+
+./replace.sh data-photo-4.1.txt books-matrix-4.1.html
+./replace.sh data-photo-4.2.txt books-matrix-4.2.html
+./replace.sh data-photo-4.3.txt books-matrix-4.3.html
+./replace.sh data-photo-4.4.txt books-matrix-4.4.html
+./replace.sh data-photo-4.5.txt books-matrix-4.5.html
+./replace.sh data-photo-4.6.txt books-matrix-4.6.html
+./replace.sh data-photo-4.7.txt books-matrix-4.7.html
+./replace.sh data-photo-4.8.txt books-matrix-4.8.html
+./replace.sh data-photo-4.9.txt books-matrix-4.9.html
+./replace.sh data-photo-4.10.txt books-matrix-4.10.html
+
+./replace.sh data-photo-5.1.txt books-matrix-5.1.html
+./replace.sh data-photo-5.2.txt books-matrix-5.2.html
+./replace.sh data-photo-5.3.txt books-matrix-5.3.html
+./replace.sh data-photo-5.4.txt books-matrix-5.4.html
+./replace.sh data-photo-5.5.txt books-matrix-5.5.html
+./replace.sh data-photo-5.6.txt books-matrix-5.6.html
+./replace.sh data-photo-5.7.txt books-matrix-5.7.html
+./replace.sh data-photo-5.8.txt books-matrix-5.8.html
+./replace.sh data-photo-5.9.txt books-matrix-5.9.html
+./replace.sh data-photo-5.10.txt books-matrix-5.10.html
+
+./replace.sh data-photo-6.1.txt books-matrix-6.1.html
+./replace.sh data-photo-6.2.txt books-matrix-6.2.html
+./replace.sh data-photo-6.3.txt books-matrix-6.3.html
+./replace.sh data-photo-6.4.txt books-matrix-6.4.html
+./replace.sh data-photo-6.5.txt books-matrix-6.5.html
+./replace.sh data-photo-6.6.txt books-matrix-6.6.html
+./replace.sh data-photo-6.7.txt books-matrix-6.7.html
+./replace.sh data-photo-6.8.txt books-matrix-6.8.html
+./replace.sh data-photo-6.9.txt books-matrix-6.9.html
+./replace.sh data-photo-6.10.txt books-matrix-6.10.html
+
+./replace.sh data-photo-7.1.txt books-matrix-7.1.html
+./replace.sh data-photo-7.2.txt books-matrix-7.2.html
+./replace.sh data-photo-7.3.txt books-matrix-7.3.html
+./replace.sh data-photo-7.4.txt books-matrix-7.4.html
+./replace.sh data-photo-7.5.txt books-matrix-7.5.html
+./replace.sh data-photo-7.6.txt books-matrix-7.6.html
+./replace.sh data-photo-7.7.txt books-matrix-7.7.html
+./replace.sh data-photo-7.8.txt books-matrix-7.8.html
+./replace.sh data-photo-7.9.txt books-matrix-7.9.html
+./replace.sh data-photo-7.10.txt books-matrix-7.10.html
+
---
+++ demo-1/photos/books/replace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+input="$1"
+html="$2"
+##
+lines_array=(10 19 43 52 76 85 109 118 142 151 175 184)
+i=-1
+##
+while IFS= read -r line
+do
+  i=$((i+1))
+  echo "${lines_array[$i]} = $line"
+  current_line=${lines_array[$i]}
+  sed -i ".bak" "${current_line}s|.*|$line|" $html 
+  ##
+done < "$input"
+##
