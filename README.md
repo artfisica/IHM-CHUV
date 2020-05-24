a3ad1595592143389f0166700c82fb0032984184
Arturos Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2020-05-24 14:45:06 +0200

    re-arrange  May 2020

--- README.md
+++
@@ -1,4 +0,0 @@
-# IHM
-
-* **February 8th, 2019** Initial commit of the project to GitHub
-* **February 15th, 2019** Moving HOME pages to PHP to read text file containing pictures' information
---
+++ us.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <?php include('header-us.html'); ?>
+</header>
+
+<!-- body -->
+<body>
+		  <ul class="bkg">
+        <!-- <table> -->
+        <?php include('people.html'); ?>
+        <!-- </table> -->
+		 </ul>
+</body>
+<!-- end main body -->
+
+<footer>
+  <?php include('footer.html'); ?>
+</footer>
+</html>
