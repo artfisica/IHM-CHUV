75136c35a3e965cc9c069bde0d48cee534545e27
Arturos Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2020-06-04 18:02:18 +0200

    redirect to home all not valid photos entries

--- photos/photo.php
+++ photos/photo.php
@@ -7,9 +7,23 @@
   <?php include('header.html'); ?>
 </header>
 
+<?php
+    $photos = array(1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 1.10, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 2.10, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 3.10, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 4.10, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 5.10, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 6.10, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 7.10);
+    
+    if (in_array($_GET["photo"],$photos))
+    {
+        /* good - continue */
+    }
+    else
+    {
+        header("Location: ../index.php"); /* Redirect browser to HOME */
+        exit();
+    }
+?>
+
 <!-- body -->
 <body>
-  <div class="macrocontainer">
+    <div class="macrocontainer">
       <div class="left">
         <a href="#"> <!-- it goes to the link in Wikidata -->
               <?php echo "<img src=\"images/photo/" . $_GET["photo"] . ".jpg\" width=\"100%\">"; ?>
