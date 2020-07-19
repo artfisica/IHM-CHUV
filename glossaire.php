20aa6c407c554a8e46f338176d3b4250ec742c90
Arturos Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2020-07-19 03:11:23 +0200

    adding related items

---
+++ glossaire.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <?php include('header-glossaire.html'); ?>
+</header>
+
+<!-- body -->
+<body>
+		  <ul class="bkg">
+        <!-- <table> -->
+        <?php include('glossaire.html'); ?>
+        <!-- </table> -->
+		 </ul>
+</body>
+<!-- end main body -->
+
+<footer>
+  <?php include('footer.html'); ?>
+</footer>
+</html>
--- photos/photo.php
+++ photos/photo.php
@@ -9,7 +9,7 @@
 
 <?php
     $photos = array(1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 1.10, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 2.10, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 3.10, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 4.10, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 5.10, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 6.10, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 7.10);
-    
+
     if (in_array($_GET["photo"],$photos))
     {
         /* good - continue */
@@ -27,11 +27,19 @@
       <div class="left">
         <a href="#"> <!-- it goes to the link in Wikidata -->
               <?php echo "<img src=\"images/photo/" . $_GET["photo"] . ".jpg\" width=\"100%\">"; ?>
+              <?php
+                  $cires = array(1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8);
+
+                  if (in_array($_GET["photo"],$cires))
+                  {
+                     echo "<a style=\"color:white;font-size:150%;padding: 0px 0px 0px 7px;\" class=\"fa\" href=\"../glossaire.php\">&#xf05a;</a>";
+                  }
+             ?>
         </a>
       </div>
       <div class="right">
         <!-- related Books -->
-        <?php include("books/books-matrix-" . $_GET["photo"] . ".html"); ?> <!-- books-matrix-" . $_GET["photo"] . ".html"); ?> -->
+        <?php include("books/books-matrix-" . $_GET["photo"] . ".html"); ?>
         <!-- end related Books -->
       </div>
       <div class="middle">
@@ -49,14 +57,14 @@
         echo "<p style=\"padding: 0px 0px 0px 10%; color:white;\">";
         echo "<b>$data[3]</b>";
         echo"<br>$data[5]</br>";
-        echo "<a href=\"https://creativecommons.org/licenses/by-sa/4.0\" title=\"Creative Commons Attribution-Share Alike 4.0\">CC BY-SA 4.0</a>";
+        echo "<a href=\"https://creativecommons.org/licenses/by-sa/4.0\" target=\"_blank\" title=\"Creative Commons Attribution-Share Alike 4.0\" >CC BY-SA 4.0</a>";
         echo "</p>";
         ?>
         <!-- end legend under the picture -->
       </div>
       <div class="below">
         <!-- related photos -->
-        <?php include("relates/related-books-" . $_GET["photo"] . ".html"); ?> <!-- related-books.html"); ?> -->
+        <?php include("relates/related-books-" . $_GET["photo"] . ".html"); ?>
         <!-- end related photos -->
       </div>
   </div>
